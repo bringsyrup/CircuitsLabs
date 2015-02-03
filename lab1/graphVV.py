5fe02512c0def22fe18428930360b9dbb35531d3
bringsyrup <rubymspring@gmail.com>
2015-02-03 18:57:09 -0500

    fixed graphVV.py, added eps

--- lab1/graphVV.py
+++ lab1/graphVV.py
@@ -20,8 +20,9 @@ def makeGraph(CSV):
 if __name__=="__main__":
 
     Vin, Vout = makeGraph(sys.argv[1])
-    plt.plot(Vout, Vin, 'ro', linewidth=2)
-    plt.xlabel("Vout", fontsize=14)
-    plt.ylabel("Vin", fontsize=14)
+    plt.plot(Vin, Vout, 'ro', linewidth=2)
+    plt.xlabel("Vin", fontsize=14)
+    plt.ylabel("Vout", fontsize=14)
+    plt.grid("on")
     plt.title("Transfer Function of Voltage Divider", fontsize=20)
     plt.show()
