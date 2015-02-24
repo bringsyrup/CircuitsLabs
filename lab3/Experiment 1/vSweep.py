86495f80348e1b56e86b0337e3cf9444b9c83013
bringsyrup <rubymspring@gmail.com>
2015-02-24 18:20:38 -0500

    added lab3

---
+++ lab3/Experiment 1/vSweep.py
@@ -0,0 +1,26 @@
+#! /bin/env python
+import smu
+
+def linspace(initial, final, n = 100):
+    if n>=2:
+        increment = (float(final) - float(initial))/(n - 1)
+        return [float(initial) + i*increment for i in range(n)]
+    else:
+        return []
+
+s = smu.smu()
+v = linspace(0.4, 0.71, 101)
+f = open('bjt.csv', 'w')
+f.write('"V","I"\n')
+
+V = []
+I = []
+for val in v:
+    s.set_voltage(1, val)
+    s.autorange(1)
+    s.autorange(2)
+    f.write('{!s},{!s}\n'.format(val, s.get_current(2)))
+
+s.set_voltage(1, 0.)
+s.set_voltage(2, 0.)
+f.close()
---
+++ lab3/Experiment 2/vRDsweep.py
@@ -0,0 +1,25 @@
+#! /bin/env python
+import smu
+
+def linspace(initial, final, n=101):
+    if n>=2:
+        increment = (float(final) - float(initial))/(n - 1)
+        return [float(initial) + i*increment for i in range(n)]
+    else:
+        return []
+
+s = smu.smu()
+v = linspace(0, 3) 
+f = open('vRDsweep3.csv', 'w')
+f.write('"Vin","Iin","V"\n')
+s.set_current(1, 0.)
+
+for val in v:
+    s.set_voltage(2, val)
+    s.autorange(1)
+    s.autorange(2)
+    f.write('{!s},{!s},{!s}\n'.format(val, s.get_current(2), s.get_voltage(1)))
+
+s.set_voltage(1, 0.)
+s.set_voltage(2, 0.)
+f.close()
