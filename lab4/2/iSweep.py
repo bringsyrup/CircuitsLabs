30ab3eec5666f5bbeed2163cbebf6e88a080ac02
bringsyrup <rubymspring@gmail.com>
2015-03-08 13:21:11 -0400

    made full test script for exp. 1 and 2

--- lab4/2/iSweep.py
+++ lab4/2/iSweep.py
@@ -1,28 +1,38 @@
 #! /bin/env python
 #for each BJT, connect collector to 5V 
 import smu
-import numpy as np
+from numpy import logspace
+import os, sys, select
 
 s = smu.smu()
-current = np.logspace(-9, -2, 101)
+current = logspace(-9, -2, 101)
 f = open('exp2.csv', 'w')
 sweep_current = ['Ix', 'Iy']
 
+def test_source(trial):
+    raw_input("Press enter when ready to test source current " + str( sweep_current[source_index]) + ' with chanel 1 for trial ' + str(trial))
+    while True:
+        #os.system('clear')
+        print s.get_current(1)
+        if sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
+            line = raw_input()
+            break
+
 for j in xrange(2): # two configurations, one where Ix is sweeping for three values of Iy, and one where Iy is sweeping for three values of Ix
-    raw_input("Press enter when ready to sweep " + sweep_current[j])
+    raw_input("Press enter when ready to begin testing for input " + sweep_current[j])
     if j == 0:
+        source_index = j+1
         f.write('Sweeping current ' + str(sweep_current[j]) + ' for 3 values of ' + str(sweep_current[j+1]) + '\n')
     else:
+        source_index = j-1
         f.write('Sweeping current ' + str(sweep_current[j]) + ' for 3 values of ' + str(sweep_current[j-1]) + '\n')
 
-    for i in xrange(3): # three values of non-sweeping input current determined by potentiometer
-        raw_input("Press enter when ready to measure " + str(i + 1))
-        f.write('Run ' + str(i+1) + ' for ' + str(sweep_current[j]) + ' sweep\n')
+    for i in xrange(3): # three values of source current determined by potentiometer
+        test_source(i + 1)
 
-        if sweep_current[j] == 'Ix':
-            f.write('Ix, Iz\n')
-        else:
-            f.write('Iy, Iz\n')
+        raw_input("Press enter to measure trial " + str(i + 1))
+        f.write('Trial ' + str(i + 1) + ' for input current ' + str(sweep_current[j])+ '\n')
+        f.write(str(sweep_current[j]) + ', Iz\n')
 
         for val in current:
             s.set_current(1, val)
