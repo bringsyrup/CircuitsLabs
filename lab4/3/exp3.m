2d23982d87f131b76e5bb11cc781d54a8326ea6d
Antonia Elsen <antoniaelsen@gmail.com>
2015-03-10 04:57:34 -0400

    this graph is fuckety

--- lab4/3/exp3.m
+++ lab4/3/exp3.m
@@ -1,14 +1,17 @@
 clf
 fontsize = 14;
+Ix1 = Ix_small;
 
-Iz2_th = (Ix1.^2) / .000000001 
-%Iz3_th = (Ix1^2 ./ ones(1,length(Iz2)).*.00016)
-%Iz4_th = (Ix1^2 ./ ones(1,length(Iz2)).*.00002)
 
-loglog(Ix1, [Izwith15M, Izwith301k, Izwith47k], 'o'); hold on
+Iz2_th = .01 ./ Ix1; 
+%Iz3_th = (Ix1^2 / .00016)
+%Iz4_th = (Ix1^2 / .00002)
+
+
+%plot(Ix1, [Iz_small, Iz_med, Iz_large], 'o'); hold on
 plot(Ix1, Iz2_th, '-k')
 %plot(Ix1, Iz3_th, '-k')
 %plot(Ix1, Iz4_th, '-k')
-title('Output Iz as a function if input Ix', 'fontsize', fontsize)
+title('Output Iz as a function of Input Ix', 'fontsize', fontsize)
 xlabel('Ix (A)', 'fontsize', fontsize)
 ylabel('Iz (A)', 'fontsize', fontsize)
