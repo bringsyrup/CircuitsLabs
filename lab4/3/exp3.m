d917fe609b25df32e20e905896d77d047000da72
Antonia Elsen <antoniaelsen@gmail.com>
2015-03-10 05:32:27 -0400

    add Iz vs Iy graph

--- lab4/3/exp3.m
+++ lab4/3/exp3.m
@@ -1,17 +1,16 @@
 clf
 fontsize = 14;
-Ix1 = Ix_small;
 
 
-Iz2_th = .01 ./ Ix1; 
+Iz2_th = 5 / -1*Ix_small; 
 %Iz3_th = (Ix1^2 / .00016)
 %Iz4_th = (Ix1^2 / .00002)
 
 
-%plot(Ix1, [Iz_small, Iz_med, Iz_large], 'o'); hold on
-plot(Ix1, Iz2_th, '-k')
-%plot(Ix1, Iz3_th, '-k')
-%plot(Ix1, Iz4_th, '-k')
+%plot(Ix_small, [Iz_small, Iz_med, Iz_large], 'o'); hold on
+plot(Ix_small, Iz2_th, '-k')
+%plot(Ix_small, Iz3_th, '-k')
+%plot(Ix_small, Iz4_th, '-k')
 title('Output Iz as a function of Input Ix', 'fontsize', fontsize)
 xlabel('Ix (A)', 'fontsize', fontsize)
 ylabel('Iz (A)', 'fontsize', fontsize)
