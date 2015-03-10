15a5a26266150df8efab0dbcbc0984b57c40e3bd
Ruby Spring <rubymspring@gmail.com>
2015-03-10 01:28:22 -0400

    added excel work books

---
+++ lab4/2/exp2.m
@@ -0,0 +1,5 @@
+fontsize = 14
+plot(Ix1, [Iz2, Iz3, Iz4], 'o')
+title('Output Iz as a function if input Ix', 'fontsize', fontsize)
+xlabel('Ix (A)', 'fontsize', fontsize)
+ylabel('Iz (A)', 'fontsize', fontsize)
