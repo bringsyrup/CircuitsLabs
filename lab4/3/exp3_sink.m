b87bfe0f3c191997cfd5f1cd9e8abaf025057b43
Ruby Spring <rubymspring@gmail.com>
2015-03-10 03:30:13 -0400

    fixed graphs, added sink graph for exp3

---
+++ lab4/3/exp3_sink.m
@@ -0,0 +1,14 @@
+clf
+fontsize = 14;
+
+Iz2_th = Ix1.^2 ./ .0009 
+Iz3_th = Ix1.^2 ./ .00012
+Iz4_th = Ix1.^2 ./ .000025
+
+plot(Ix1, [Iz1_exp3, Iz2_exp3, Iz3_exp3], 'o'); hold on
+plot(Ix1(1:96), Iz2_th(1:96), '-k')
+plot(Ix1(1:90), Iz3_th(1:90), '-k')
+plot(Ix1(1:85), Iz4_th(1:85), '-k')
+title('Output Iz as a function if input Ix', 'fontsize', fontsize)
+xlabel('Ix (A)', 'fontsize', fontsize)
+ylabel('Iz (A)', 'fontsize', fontsize)
