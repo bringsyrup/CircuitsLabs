7229529ae664ab3a148c5d47f61ea1070227c312
bringsyrup <rubymspring@gmail.com>
2015-03-24 22:01:41 -0400

    actual data for nmos and good data for pmos

--- lab5/2/vSweep.py
+++ lab5/2/vSweep.py
@@ -11,15 +11,15 @@ def linspace(initial, final, n = 100):
 
 s = smu.smu()
 v = linspace(0., 5., 151)
-f = open('nMOS_exp2.csv', 'w')
+f = open('pMOS_exp2.csv', 'w')
 f.write('V_S, I_channel')
 s.set_voltage(2, 0.)
 
 for val in v:
     s.set_voltage(1, val)
     s.autorange(1)
-    print s.get_current(2)*-1.
-    f.write('{!s},{!s}\n'.format(s.get_voltage(1), s.get_current(2)*-1.))
+    print s.get_current(2)#*-1.
+    f.write('{!s},{!s}\n'.format(s.get_voltage(1), s.get_current(2)))
 s.set_voltage(1, 0.)
 s.set_voltage(2, 0.)
 
