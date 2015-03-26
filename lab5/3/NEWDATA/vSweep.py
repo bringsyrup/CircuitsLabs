248228508bf531f35828fd0a5374f2d029869a07
bringsyrup <rubymspring@gmail.com>
2015-03-26 19:48:47 -0400

    added all new data for exp3

---
+++ lab5/3/NEWDATA/vSweep.py
@@ -0,0 +1,28 @@
+#! /bin/env python
+#for each BJT, connect collector to 5V 
+import smu
+
+def linspace(initial, final, n = 100):
+    if n>=2:
+        increment = (float(final) - float(initial))/(n - 1)
+        return [float(initial) + i*increment for i in range(n)]
+    else:
+        return []
+
+s = smu.smu()
+v = linspace(-5., 0., 151)
+f = open('pMOS_exp3_-1V.csv', 'w')
+f.write('V_DS, I_channel\n')
+#s.autorange(2)
+s.set_voltage(2, -1)
+#s.set_current(2, 0.)
+
+for val in v:
+    s.set_voltage(1, val)
+    s.autorange(1)
+    print s.get_current(1)#*-1.
+    f.write('{!s},{!s}\n'.format(s.get_voltage(1), s.get_current(1)))
+s.set_voltage(1, 0.)
+s.set_voltage(2, 0.)
+
+f.close()
