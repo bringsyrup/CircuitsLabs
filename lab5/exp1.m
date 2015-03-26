749e0a7f90266e9684f743153291dc36c7ebae01
Antonia Elsen <antoniaelsen@gmail.com>
2015-03-25 23:20:34 -0400

    exp1 matlab code, graphs

--- lab5/exp1.m
+++ lab5/exp1.m
@@ -1,5 +1,32 @@
+%Experiment 1 --------------------------------------
+%NMOS:
+% hold all;
+% epsilon = 2*10^(-4);
+% [Is, VT, kappa] = ekvfit(V_NMOS(15:end), I_NMOS(15:end), epsilon, ['on'])
+%PMOS:
+hold all;
+epsilon = 2*10^(-4);
+[Is, VT, kappa] = ekvfit(V_NMOS(15:end), I_NMOS(15:end), epsilon, ['on'])
+
+
+%Transconductance gain: ----------------------------
 clf;
-clc;
 hold all;
 
-[Is, VT, kappa] = ekvfit(V_G, I_channel00, 5*10^-5)
+
+% Is = 1.9633*10^(-6);
+% Ut = .025; %Thermal voltage is 25 mV
+% k = .2058;
+% Wstart = 50;
+% Wend = 65;
+% Sstart = Wend + 50;
+% %Transconductance from Data
+% loglog(I_NMOS(2:end), diff(I_NMOS) ./ diff(V_NMOS));
+% %Theoretical Weak Inversion
+% loglog(I_NMOS(Wstart:Wend), (I_NMOS(Wstart:Wend)/Ut)*k);
+% %Theoretical Strong Inversion
+% loglog(I_NMOS(Sstart:end), sqrt(Is*I_NMOS(Sstart:end)/Ut)*k);
+% legend('Transconductance Gain', 'G_m Weak Inversion', 'G_m Strong Inversion')
+% title('Transconductance Gain of an NMOS Transistor','FontSize',14)
+% xlabel('Current (A)')
+% ylabel('Transconductance Gain (Mhos)')
