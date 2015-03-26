5c563b4001a9d4a5fabbbda190b584cb02dd8122
bringsyrup <rubymspring@gmail.com>
2015-03-26 19:38:26 -0400

    added new data

--- lab5/1/NEWDATA/vSweep.py
+++ lab5/1/NEWDATA/vSweep.py
@@ -10,18 +10,17 @@ def linspace(initial, final, n = 100):
         return []
 
 s = smu.smu()
-v = linspace(0., 5., 151)
-f = open('nMOS_exp3_800mV.csv', 'w')
-f.write('V_DS, I_channel\n')
-#s.autorange(2)
-s.set_voltage(2, .8)
-#s.set_current(2, 0.)
+v = linspace(0., 5., 301)
+f = open('nMOS_exp1.csv', 'w')
+f.write('V_G, I_channel\n')
+s.set_voltage(2, 0.)
 
 for val in v:
     s.set_voltage(1, val)
     s.autorange(1)
-    print s.get_current(1)#*-1.
-    f.write('{!s},{!s}\n'.format(s.get_voltage(1), s.get_current(1)))
+    s.autorange(2)
+    print s.get_current(2)*-1
+    f.write('{!s},{!s}\n'.format(s.get_voltage(1), s.get_current(2)*-1))
 s.set_voltage(1, 0.)
 s.set_voltage(2, 0.)
 
--- lab5/2/NEWDATA/vSweep.py
+++ lab5/2/NEWDATA/vSweep.py
@@ -10,18 +10,16 @@ def linspace(initial, final, n = 100):
         return []
 
 s = smu.smu()
-v = linspace(0., 5., 151)
-f = open('nMOS_exp3_800mV.csv', 'w')
-f.write('V_DS, I_channel\n')
-#s.autorange(2)
-s.set_voltage(2, .8)
-#s.set_current(2, 0.)
+v = linspace(0., 3, 100) + linspace(3.02, 3.5, 40) + linspace(3.5008, 3.7, 75)
+f = open('nMOS_exp2.csv', 'w')
+f.write('V_S, I_channel\n')
+#s.set_voltage(2, -3.)
 
 for val in v:
     s.set_voltage(1, val)
     s.autorange(1)
-    print s.get_current(1)#*-1.
-    f.write('{!s},{!s}\n'.format(s.get_voltage(1), s.get_current(1)))
+    print -1*s.get_current(1), s.get_voltage(1)#*-1.
+    f.write('{!s},{!s}\n'.format(s.get_voltage(1), -1*s.get_current(1)))
 s.set_voltage(1, 0.)
 s.set_voltage(2, 0.)
 
