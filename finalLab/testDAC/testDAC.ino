56a2bf652706715ca4dd6804ad6d56004fabd20f
Thomas Nattestad <thomas.nattestad@gmail.com>
2015-05-06 18:54:10 -0400

    more data

--- finalLab/testDAC/testDAC.ino
+++ finalLab/testDAC/testDAC.ino
@@ -2,7 +2,7 @@ float val = 0;           // variable to store the value read
 
 void setup() {
   Serial.begin(9600); 
-  for(int i = 1; i < 5; i++){
+  for(int i = 1; i < 14; i++){
     pinMode(i, OUTPUT);
   }
 }
@@ -10,8 +10,8 @@ void setup() {
 void loop() {
   digitalWrite(2, HIGH);
   digitalWrite(3, LOW);
-  digitalWrite(4, LOW);
-  digitalWrite(5, LOW);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, HIGH);
   
   val = analogRead(0);    // read the input pin
   Serial.println(val/1023*5);             // debug value
@@ -23,20 +23,20 @@ void loop() {
 //  }
 }
 
-//0 = 0.00
-//1 = 0.70
-//2 = 
-//3 = 
-//4 = 
-//5 = 
-//6 = 
-//7 = 
-//8 = 
-//9 = 
-//10 = 
-//11 = 
-//12 = 
-//13 = 
-//14 = 
-//15 = 
+//0  = 0.00
+//1  = 0.70
+//2  = 0.33
+//3  = 1.00
+//4  = 1.33
+//5  = 1.67
+//6  = 2.00
+//7  = 2.33
+//8  = 2.66
+//9  = 3.00
+//10 = 3.33
+//11 = 3.67
+//12 = 4.00
+//13 = 4.34
+//14 = 4.67
+//15 = 5.00
 
